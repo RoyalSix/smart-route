08cd0fb286436cca73302722a4d61e1353c1a73c
RoyalSix <jay.a.scottjr@biola.edu>
2019-07-21 09:17:17 -0700

    setting context

--- src/js/components/RoutingMap/index.js
+++ src/js/components/RoutingMap/index.js
@@ -11,10 +11,60 @@ export class RoutingMap extends Component {
     this.platform = new H.service.Platform({
       'apikey': 'oJoZvCU0vs1cfOTzlU1TL8PJcUVyem2fKBvS1eHfh2k'
     });
+    this.setRoute = this.setRoute.bind(this);
+  }
+  setRoute(result) {
+    var route,
+      routeShape,
+      startPoint,
+      endPoint,
+      linestring;
+    if (result.response.route) {
+      // Pick the first route from the response:
+      route = result.response.route[0];
+      // Pick the route's shape:
+      routeShape = route.shape;
+
+      // Create a linestring to use as a point source for the route line
+      linestring = new H.geo.LineString();
+
+      // Push all the points in the shape into the linestring:
+      routeShape.forEach(function(point) {
+        var parts = point.split(',');
+        linestring.pushLatLngAlt(parts[0], parts[1]);
+      });
+
+      // Retrieve the mapped positions of the requested waypoints:
+      startPoint = route.waypoint[0].mappedPosition;
+      endPoint = route.waypoint[1].mappedPosition;
+
+      // Create a polyline to display the route:
+      var routeLine = new H.map.Polyline(linestring, {
+        style: {strokeColor: 'green', lineWidth: 2}
+      });
+
+      // Create a marker for the start point:
+      var startMarker = new H.map.Marker({
+        lat: startPoint.latitude,
+        lng: startPoint.longitude
+      });
+
+      // Create a marker for the end point:
+      var endMarker = new H.map.Marker({
+        lat: endPoint.latitude,
+        lng: endPoint.longitude
+      });
+
+      // Add the route polyline and the two markers to the map:
+      this.map.addObjects([routeLine, startMarker, endMarker]);
+
+      // Set the map's viewport to make the whole route visible:
+      this.map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
+      this.map.setZoom(this.map.getZoom() + -0.5, true);
+    }
   }
   setMap(originAddress, destAddress) {
     if (originAddress && destAddress) {
-      debugger;
       if (this.map) {
         const mapObjects = this.map.getObjects();
         mapObjects.forEach((obj) => {
@@ -46,65 +96,13 @@ export class RoutingMap extends Component {
         'representation': 'display'
       };
 
-      // Define a callback function to process the routing response:
-      var onResult = function(result) {
-        var route,
-          routeShape,
-          startPoint,
-          endPoint,
-          linestring;
-        if (result.response.route) {
-          // Pick the first route from the response:
-          route = result.response.route[0];
-          // Pick the route's shape:
-          routeShape = route.shape;
-
-          // Create a linestring to use as a point source for the route line
-          linestring = new H.geo.LineString();
-
-          // Push all the points in the shape into the linestring:
-          routeShape.forEach(function(point) {
-            var parts = point.split(',');
-            linestring.pushLatLngAlt(parts[0], parts[1]);
-          });
-
-          // Retrieve the mapped positions of the requested waypoints:
-          startPoint = route.waypoint[0].mappedPosition;
-          endPoint = route.waypoint[1].mappedPosition;
-
-          // Create a polyline to display the route:
-          var routeLine = new H.map.Polyline(linestring, {
-            style: {strokeColor: 'green', lineWidth: 2}
-          });
-
-          // Create a marker for the start point:
-          var startMarker = new H.map.Marker({
-            lat: startPoint.latitude,
-            lng: startPoint.longitude
-          });
-
-          // Create a marker for the end point:
-          var endMarker = new H.map.Marker({
-            lat: endPoint.latitude,
-            lng: endPoint.longitude
-          });
-
-          // Add the route polyline and the two markers to the map:
-          this.map.addObjects([routeLine, startMarker, endMarker]);
-
-          // Set the map's viewport to make the whole route visible:
-          this.map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
-          this.map.setZoom(this.map.getZoom() + -0.5, true);
-        }
-      };
-
       // Get an instance of the routing service:
       var router = this.platform.getRoutingService();
 
       // Call calculateRoute() with the routing parameters,
       // the callback and an error callback function (called if a
       // communication error occurs):
-      router.calculateRoute(routingParameters, onResult,
+      router.calculateRoute(routingParameters, this.setRoute,
         function(error) {
           alert(error.message);
         });
